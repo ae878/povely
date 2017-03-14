425ea6f8589f2eb19fa5ad5690e09d98f2158864
Jihoon Koo <koo2788@naver.com>
2017-03-14 23:56:54 +0900

    다이어리 생성 작업중

--- app/src/main/java/com/example/mimi/povely/DiaryList.java
+++ app/src/main/java/com/example/mimi/povely/DiaryList.java
@@ -1,7 +1,10 @@
 package com.example.mimi.povely;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Koo on 2017-03-13.
@@ -14,5 +17,15 @@ public class DiaryList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diary_list);
+
+        Button to_diary_new = (Button)findViewById(R.id.to_diary_new);
+
+        to_diary_new.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Diarynew.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mimi/povely/Diarynew.java
@@ -0,0 +1,16 @@
+package com.example.mimi.povely;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Koo on 2017-03-14.
+ */
+
+public class Diarynew extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diary_new);
+    }
+}
