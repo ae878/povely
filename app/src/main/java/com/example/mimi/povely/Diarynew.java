5c49d1b81d9e7ba7b2841638835765f67dc1d91e
Jihoon Koo <koo2788@naver.com>
2017-03-15 14:54:31 +0900

    koo

--- app/src/main/java/com/example/mimi/povely/Diarynew.java
+++ app/src/main/java/com/example/mimi/povely/Diarynew.java
@@ -2,6 +2,8 @@ package com.example.mimi.povely;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Koo on 2017-03-14.
@@ -12,5 +14,12 @@ public class Diarynew extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.diary_new);
+        Button go_back = (Button)findViewById(R.id.go_back_in_diary_new);
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
