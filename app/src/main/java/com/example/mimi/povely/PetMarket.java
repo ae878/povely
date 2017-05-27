06d1eb32cad438418510e238b3b18c5735a90c62
jsy <jsangyun1039@naver.com>
2017-05-27 13:51:08 +0900

    hmm

--- app/src/main/java/com/example/mimi/povely/PetMarket.java
+++ app/src/main/java/com/example/mimi/povely/PetMarket.java
@@ -3,18 +3,25 @@ package com.example.mimi.povely;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 /**
  * Created by JSY on 2017-04-03.
  */
 
 public class PetMarket extends BaseActivity{
+
+    ListView itemList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pet_market);
 
         getWindow().setWindowAnimations(0); // acitivity 효과 없애기
+
+        itemList = (ListView)findViewById(R.id.item_list);
+        itemList = null;
 
     }
 
